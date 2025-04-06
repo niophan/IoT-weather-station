a935e1cbe9cf6c63b94e328c7ac8eb78559aa77b
Nhan Phan <niophan.io@gmail.com>
2025-04-06 19:30:55 +0300

    Update Arduino-Lampotila.ino
    update Adruino-koodi

--- Adruino Koodit/Arduino-Lampotila/Arduino-Lampotila.ino
+++ Adruino Koodit/Arduino-Lampotila/Arduino-Lampotila.ino
@@ -1,13 +1,19 @@
 #include <LiquidCrystal.h>
 
 const float vRef = 5.0;
-const float analogPin = A0;
+const int analogPin = A0;
+const int signalPin = 2;
+volatile unsigned long pulseCount = 0;
 
-
+// Opettajan-avun koodit
 // int d7 = 2, d6 = 3, d5 = 4, d4 = 5, enable = 11, rs = 12;
 // LiquidCrystal lcd(rs, enable, d4, d5, d6, d7);
 
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+// byte a_umlaut[8] = { B01110, B00000, B01110, B10001, B11111, B10001, B10001, B00000 }; // ä
+// byte o_umlaut[8] = { B01110, B00000, B01110, B10001, B10001, B10001, B01110, B00000 }; // ö
+// byte a_ring[8]    = { B00100, B01010, B00100, B01110, B10001, B11111, B10001, B00000 }; // å
+
+LiquidCrystal lcd(9, 8, 7, 6, 5, 4);
 byte heart[8] = {
   0B00000,
   0B01010,
@@ -19,11 +25,23 @@ byte heart[8] = {
   0B00000,
 };
 
+void countPulse() {
+  pulseCount++;
+}
+
+
 
 void setup() {
-  // Serial.begin(9600);
+  Serial.begin(9600);
+  pinMode(signalPin, INPUT);
+  attachInterrupt(digitalPinToInterrupt(signalPin), countPulse, RISING);
   lcd.createChar(3, heart);
-  lcd.begin(19, 4);
+  // lcd.createChar(0, a_umlaut);
+  // lcd.createChar(1, o_umlaut);
+  // lcd.createChar(2, a_ring);
+
+
+  lcd.begin(20, 4);
   // lcd.write(byte(0));
   lcd.print(" Me \x03 sulari ");
   lcd.setCursor(1, 1);
@@ -33,6 +51,9 @@ void setup() {
   delay(2500);
   lcd.clear();
 
+  lcd.print("Humidity Sensor");
+  delay(2000);
+  lcd.clear();
 }
 
 void loop() {
@@ -40,28 +61,47 @@ void loop() {
    const float raw = analogRead(analogPin);
    const float voltage = rawToVoltage(vRef, raw);
    const float temp = voltageToTampere(voltage);
+   const float frequency = readFrequency();
+   const float humidity = frequencyToHumidity(frequency);
    
-   // Tulosta säädata
-   lcd.print("ADC value: " + String(raw));
-   delay(2500);
+   // Serial Monitor -
+   Serial.println("-----------------------------");
+   Serial.print("ADC: ");
+   Serial.println(raw);
+   Serial.print("Voltage: ");
+   Serial.print(voltage);
+   Serial.println(" V");
+   Serial.print("Temp: ");
+   Serial.print(temp);
+   Serial.println(" C degrees");
+   Serial.print("Frequency: ");
+   Serial.print(frequency);
+   Serial.print(" Hz, Humidity: ");
+   Serial.print(humidity);
+   Serial.println("%");
+   Serial.println("-----------------------------");
+
+   // LDC
+   lcd.setCursor(0, 0);
+   lcd.print("ADC:" + String(raw));
+   lcd.setCursor(0, 1);
+   lcd.print("Voltage:" + String(voltage) + " V");
+   lcd.setCursor(0, 2);
+   lcd.print("Temp:" + String(temp) + " C degree");
+   
+   delay(3500);
    lcd.clear();
 
    lcd.setCursor(0, 0);
-   lcd.print("Voltage value: " + String(voltage));
+   lcd.print("Freq: " + String(frequency) + " Hz");
    lcd.setCursor(0, 1);
-   lcd.print(" voltages.");
-   delay(2500);
-   lcd.clear();
+   lcd.print("Humidity: " + String(humidity) + "%");
 
-   lcd.setCursor(0, 0);
-   lcd.print("Temp value: " + String(temp));
-   lcd.setCursor(0, 1);
-   lcd.print(" celcius degrees.");
-   delay(2500);
+   delay(1500); 
    lcd.clear();
    
    // Tulosta A->Z 
-    while(vRef) {
+   /* while(true) {
       for (int i = 65; i <= 90; i++) { 
         lcd.setCursor(x, y);
         lcd.write(i); 
@@ -74,20 +114,35 @@ void loop() {
         else if (y == 1 && x > 0) x--;       // Vasemmalle alhaalla
         else if (x == 0 && y > 0) y--;       // Ylös vasemmalla
       }
-    }
-    
 
-  // Serial.println("-----------------------------");
-  // Serial.println("ADC value: ");
-  // Serial.println(raw);
-  // Serial.println("Voltage value: ");
-  // Serial.println(String(voltage) + " voltages");
-  // Serial.println("Temperature value: ");
-  // Serial.println(String(temp) + " Celcius degrees");
-  
+      // Print custom characters Ä, Å, Ö
+      byte extraChars[] = { 0, 2, 1 }; 
+      for (int j = 0; j < 3; j++) {
+        lcd.setCursor(x, y);
+        lcd.write(extraChars[j]);
+        delay(300);
+        lcd.clear();
 
-  delay(3000);
+        if (x < 19 && y == 0) x++;
+        else if (x == 19 && y < 1) y++;
+        else if (y == 1 && x > 0) x--;
+        else if (x == 0 && y > 0) y--;
+    } */
+}
 
+float readFrequency() {
+  pulseCount = 0;
+  unsigned long startTime = millis();
+  while (millis() - startTime < 1000) {
+  }
+  return pulseCount;
+}
+
+float frequencyToHumidity(float frequency) {
+  frequency /= 1000.0;
+  // float humidity = 100 - ((frequency - 6.9) / (7.9 - 6.9) * 60); // Linear interpolation
+  // return humidity;
+  return 0*frequency*frequency -62.2222*frequency + 530.4444; // Nonlinear (Käyräsovitus)
 }
 
 float rawToVoltage(float vRef, float raw) {
