fdaac8dd705b91207d6bc9d6077a37d21eb2ace7
Nhan Phan <niophan.io@gmail.com>
2025-04-23 14:19:55 +0300

    fix nappaimisto-funktiot

--- Adruino Koodit/Arduino-Lampotila/Arduino-Lampotila.ino
+++ Adruino Koodit/Arduino-Lampotila/Arduino-Lampotila.ino
@@ -58,13 +58,17 @@ bool analogReady = false, freqReady = false;
 float frequency = 0;
 float avgTemp = 0;
 float avgHumid = 0;
-float lampo = 0;
-float kosteus = 0;
+float lampo, kosteus;
 
 // Tilastot for button 3
-float minAnalog = 0, maxAnalog = 0;
-unsigned long minFreq = 0, maxFreq = 0;
+float minAnalog = 1000.0;
+float maxAnalog = -1000.0;
+float minFreq = 1000.0;
+float maxFreq = -1000.0;
 bool showMin = true;
+bool statsInitialized = false;
+
+
 
 // ISR services
 void countPulse() {
@@ -73,13 +77,13 @@ void countPulse() {
 
 void timerIsr() {
   frequency = pulseCount;
+  pulseCount = 0;
   freqSignals[freqIndex] = frequency;
   freqIndex++;
   if (freqIndex >= SEKUNTI_COUNT) {
     freqIndex = 0;
     freqReady = true;
   }
-  pulseCount = 0;
 }
 
 // MQTT ja Ethernet funktiot
@@ -99,12 +103,14 @@ void send_MQTT_message(float tempAvg, float humidAvg) {
     // floatToString
     dtostrf(tempAvg, 4, 2, outStr1);
     dtostrf(humidAvg, 4, 2, outStr2);
+    sprintf(msgBuf, "IOTJS={\"S_name\":\"ENERGY_lampo\",\"S_value\":%s}", outStr1);
+    sprintf(msgBuf, "IOTJS={\"S_name\":\"ENERGY_kosteus\",\"S_value\":%s }", outStr2);
 
-    sprintf(msgBuf, "IOTJS={\"S_name\":\"ENERGY_lampo\",\"S_value\":%s,"
-      "\"S_name\":\"ENERGY_ikosteus\",\"S_value\":%s}",
-      outStr1, outStr2);
+    // sprintf(msgBuf, "IOTJS={\"S_name\":\"ENERGY_lampo\",\"S_value\":%s,"
+    //   "\"S_name\":\"ENERGY_kosteus\",\"S_value\":%s}",
+    //   outStr1, outStr2);
     client.publish(outTopic, msgBuf); // Lähetetään viesti MQTT-brokerille
-    Serial.println("Message sent to MQTT server."); Serial.println(payload);// Tulostetaan viesti onnistuneesta lähettämisestä
+    Serial.println("Message sent to MQTT server."); Serial.println(msgBuf);// Tulostetaan viesti onnistuneesta lähettämisestä
   } else {
     Serial.println("Failed to send message: not connected to MQTT server.");
   }
@@ -113,7 +119,7 @@ void send_MQTT_message(float tempAvg, float humidAvg) {
 void connect_MQTT_server() {
     Serial.println("Connecting to MQTT broker..."); // Tulostetaan vähän info-viestiä
       if (client.connect(clientId)) { // Tarkistetaan saadaanko yhteys MQTT-brokeriin
-          Serial.println("Connected OK"); // Yhdistetty onnistuneesti
+          Serial.println("Connected OK!"); // Yhdistetty onnistuneesti
       } else {
           Serial.println("Connection failed."); // Yhdistäminen epäonnistui
       } 
@@ -133,10 +139,14 @@ float voltageToTemperature(float voltage) {
 }
 
 float frequencyToHumidity(float frequency) {
-  float f_khz = frequency /= 1000.0;
-  // float humidity = 100 - ((frequency - 6.9) / (7.9 - 6.9) * 60); // Linear interpolation
-  // return humidity;
-  return 0*f_khz*f_khz -62.2222*f_khz + 530.4444; // Nonlinear (Käyräsovitus)
+  // float humid = 100 - ((frequency  - 6.9) / (7.9 - 6.9) * 60); // Linear interpolation
+  // return ;
+
+  float humid = (0 + (-62.2222 * (frequency/ 1000.0)) + 530.4444); // Käyrän sovittaminen pisteistöön
+  if(humid > 105.0) {
+    humid = 20;
+  }
+  return humid;
 }
 
 // UI funktiot
@@ -146,16 +156,17 @@ void showNetwork() {
   lcd.setCursor(0, 0);
   lcd.print("IP: "); lcd.print(Ethernet.localIP());
   lcd.setCursor(0, 1);
-  lcd.print("MQTT: "); lcd.print(client.connected() ? "Connected OK" : "ERROR happened");
+  lcd.print("MQTT: "); 
+  lcd.print((client.connect(clientId)) ? "Connected OOK!" : "ERROR happened");
 }
 
 // Nappi 2: näytä lämpön ja kosteuksen keskiarvoja
-void showSignals(float tempAvg, float humidAvg) {
+void showSignals(float avgTemp, float avgHumid) {
   lcd.clear();
   lcd.setCursor(0, 0);
-  lcd.print("AvgT: "); lcd.print(tempAvg, 1); lcd.print("C");
+  lcd.print("AvgT: "); lcd.print(avgTemp, 1); lcd.print("C");
   lcd.setCursor(0, 1);
-  lcd.print("AvgH: "); lcd.print(humidAvg, 1); lcd.print("%");
+  lcd.print("AvgH: "); lcd.print(avgHumid, 1); lcd.print("%");
 }
 
 // Nappi 3: vaihda min/max lämpö & kosteus
@@ -163,14 +174,14 @@ void showStats() {
   lcd.clear();
   if (showMin) {
     lcd.setCursor(0, 0);
-    lcd.print("Min lämpö: "); lcd.print(minAnalog);
+    lcd.print("Min lampo: "); lcd.print(minAnalog, 1);
     lcd.setCursor(0, 1);
-    lcd.print("Min kosteus: "); lcd.print(minFreq);
+    lcd.print("Min kosteus: "); lcd.print(minFreq, 1);
   } else {
     lcd.setCursor(0, 0);
-    lcd.print("Max lämpö: "); lcd.print(maxAnalog);
+    lcd.print("Max lampo: "); lcd.print(maxAnalog, 1);
     lcd.setCursor(0, 1);
-    lcd.print("Max kosteus: "); lcd.print(maxFreq);
+    lcd.print("Max kosteus: "); lcd.print(maxFreq, 1);
   }
   showMin = !showMin;
 }
@@ -179,9 +190,13 @@ void showStats() {
 void customAction(float lampo, float kosteus) {
   lcd.clear();
   lcd.setCursor(0, 0);
-  lcd.print("Temp:" + String(lampo) + " C degree");
+  lcd.print("Temp: ");
+  lcd.print(lampo, 1);
+  lcd.print("C");
   lcd.setCursor(0, 1);
-  lcd.print("Humid: " + String(kosteus) + "%");
+  lcd.print("Humid: ");
+  lcd.print(kosteus, 1);
+  lcd.print("%");
 }
 
 void handleKey(char key) {
@@ -222,10 +237,16 @@ void setup() {
 // Main loop
 void loop() {
   client.loop();
+
   const int raw = analogRead(analogPin);
   const float voltage = rawToVoltage(vRef, raw);
   const float temp = voltageToTemperature(voltage);
   const float humidity = frequencyToHumidity(frequency);
+
+  if (temp     < minAnalog) minAnalog = temp;
+  if (temp     > maxAnalog) maxAnalog = temp;
+  if (humidity > maxFreq  ) maxFreq   = humidity;
+  if (humidity < minFreq  ) minFreq   = humidity;
   
   // Signaalin lukeminen
   analogSignals[analogIndex] = raw;
@@ -234,10 +255,22 @@ void loop() {
     analogIndex = 0;
     analogReady = true;
   }
-  minAnalog = min(minAnalog, temp);
-  maxAnalog = max(maxAnalog, temp);
-  minFreq = min(minFreq, humidity);
-  maxFreq = max(maxFreq, humidity);
+
+  lampo = temp;
+  kosteus = humidity;
+
+ float analogSum = 0, freqSum = 0;
+  for (int i = 0; i < SEKUNTI_COUNT; i++) {
+    analogSum += analogSignals[i];
+    freqSum   += freqSignals[i];
+  }
+  float analogAvg  = analogSum / SEKUNTI_COUNT;
+  float freqAvg    = freqSum   / SEKUNTI_COUNT;
+  float voltageAvg = rawToVoltage(vRef, analogAvg);
+
+  avgTemp  = voltageToTemperature(voltageAvg);
+  avgHumid = frequencyToHumidity(freqAvg);  
+
 
    // Serial Monitor -
    Serial.println("-----------------------------");
@@ -247,49 +280,34 @@ void loop() {
    Serial.print("Frequency: "); Serial.print(frequency); 
    Serial.print(" Hz, Humidity: ");Serial.print(humidity); Serial.println("%");
    Serial.println("-----------------------------");
-   delay(1000); 
+
    
+   // Keypad handling
+   char key = keypad.getKey();
+   if (key) {
+   Serial.print("Key pressed: ");
+   Serial.println(key);
+   handleKey(key);
+   }
+
+
   // 10 sekunnin jälkeen 
    if(analogReady && freqReady) {
-    unsigned long now = millis();
-      float analogSum = 0, freqSum = 0;
-      for(int i = 0; i < SEKUNTI_COUNT; i++) {
-        analogSum += analogSignals[i];
-        freqSum += freqSignals[i];
-      }
-
-      float analogAvg = analogSum / SEKUNTI_COUNT;
-      float freqAvg = freqSum / SEKUNTI_COUNT;
-      float voltageAvg = rawToVoltage(vRef, analogAvg);
-      float tempAvg = voltageToTemperature(voltageAvg);
-      float humidAvg = frequencyToHumidity(freqAvg);
+      send_MQTT_message(avgTemp, avgHumid);
 
       Serial.println("=== AVERAGE OVER 10s ===");
       Serial.print("AnalogAvg: "); Serial.println(analogAvg);
       Serial.print("VolAvg: "); Serial.println(voltageAvg);
-      Serial.print("TempAvg: "); Serial.println(tempAvg);
+      Serial.print("TempAvg: "); Serial.println(avgTemp);
       Serial.print("FreqAvg: "); Serial.print(freqAvg); Serial.println(" Hz");
-      Serial.print("HumidAvg: "); Serial.print(humidAvg); Serial.println(" %");
+      Serial.print("HumidAvg: "); Serial.print(avgHumid); Serial.println(" %");
       Serial.println("========================");
-
-          // store for showSignals
-      avgTemp   = tempAvg;
-      avgHumid  = humidAvg;
-      lampo = temp;
-      kosteus = humidity;
-
-      send_MQTT_message(tempAvg, humidAvg);
 
       // RESET flags
       analogReady = freqReady =false; 
-  }
 
-  // Keypad handling
-  char key = keypad.getKey();
-  if (key) {
-    Serial.print("Key pressed: ");
-    Serial.println(key);
-    handleKey(key);
   }
+  delay(1000); 
+
 
 }
